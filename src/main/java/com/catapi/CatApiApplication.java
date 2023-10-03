2755860ac0cab77ec06272db22a10f6acf3d6b6f
dima udodik <dima udodik>
2023-10-03 22:47:35 +0300

    Init

---
+++ src/main/java/com/catapi/CatApiApplication.java
@@ -0,0 +1,13 @@
+package com.catapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatApiApplication.class, args);
+	}
+
+}
