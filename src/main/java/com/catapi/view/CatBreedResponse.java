b64905579c94921b548b1f5eaeccbf1b1b74449f
dima udodik <dima udodik>
2023-12-16 14:45:55 +0200

    Create CatBreedView and CatBreedResponse

---
+++ src/main/java/com/catapi/view/CatBreedResponse.java
@@ -0,0 +1,17 @@
+package com.catapi.view;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CatBreedResponse {
+
+    @JsonProperty("breeds")
+    private CatBreedView[] breeds;
+}
