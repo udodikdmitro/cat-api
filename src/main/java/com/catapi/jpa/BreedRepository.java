32100843cf0803fdc74c7933900507ac8a59c35e
dima udodik <dima udodik>
2024-04-15 22:55:54 +0300

    Finish fix

--- src/main/java/com/catapi/jpa/BreedRepository.java
+++ src/main/java/com/catapi/jpa/BreedRepository.java
@@ -2,13 +2,8 @@ package com.catapi.jpa;
 
 import com.catapi.entity.Breed;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
 import java.util.Optional;
-import java.util.Set;
 
 public interface BreedRepository extends JpaRepository<Breed, Long> {
     Optional<Breed> findByOuterBreedId(String outerBreedId);
-    @Query("SELECT breed.outerBreedId FROM Breed as breed")
-    Set<String> getAllOuterBreedId();
 }
